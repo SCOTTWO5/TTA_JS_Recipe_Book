9ebb6127fad5a861be3230a3ab9ffc34b0449351
SCOTTWO5 <scottidris6@gmail.com>
2024-10-17 10:04:14 +0200

    completed the layout

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
-const recipe = [
+const recipes = [
     {
         title: "Spaghetti Bolognese",
-        ingredients: "spaghetti, Ground Beef, Tomatoes Sauce, Onion, Garlic, Olive Oli",
-        steps: "1. Boil Pasta. 2.Cook Ground Beef. 3.Add Tomatoes Sauce, and Garlic. 4.Serve Over cooked pasta.", 
+        ingredients: "spaghetti, Ground Beef, Tomato Sauce, Garlic, Onion, Olive Oli",
+        steps: "1. Boil pasta. 2.Cook ground beef. 3.Add sauce and Garlic. 4.Mix with pasta."
     },
     {
         title: "Chicken Curry",
         ingredients: "Chicken, Curry Powder, Coconut Milk, Onions, Garlic, Ginger",
-        steps: "1. Cook Chicken. 2. Add Onions, Garlic, Ginger. 3. Add Coconut Milk and Curry Powder. 4. Simmer."
+        steps: "1. Cook chicken. 2. Add Onions, garlic, ginger. 3. Add Coconut Milk and Curry Powder. 4. Simmer."
     },
     {
         title: "Vegetable Stir-fry",
@@ -15,3 +15,23 @@ const recipe = [
         steps: "1. Stir-fry vegetables in olive oil. 2. Add garlic and soy sauce. 3. Serve with rice."
     }
 ];
+
+const displayRecipes = () => {
+    const recipeList = document.querySelector("#recipeList");
+    recipeList.innerHTML = "";
+
+    recipes.forEach((recipe) => {
+        const recipeCard = document.createElement("div");
+        recipeCard.classList.add("bg-white", "p-4", "rounded", "shadow", "mb-4");
+
+        recipeCard.innerHTML = `
+            <h2 class="text-lg font-bold">${recipe.title}</h2>
+            <p class="text-sm text-gray-500"><strong>Ingredients: &emsp;</strong>${recipe.ingredients}</p>
+            <p class="text-sm"><strong>Steps: &emsp;</strong>${recipe.steps}</p>}
+        `;
+        recipeList.appendChild(recipeCard);
+    })
+}
+
+
+displayRecipes();
